50c76aa9084bf26b953541c2942861d7f44c00e1
Bruna Silva <brunasilvaip2@gmail.com>
2024-12-16 15:18:08 -0300

    Reorganiza o exercicio09.js da lista 04

--- Exercicios/Exercicios/javaScript/lib/ExerciciosLista04/exercicio09.js
+++ Exercicios/Exercicios/javaScript/lib/ExerciciosLista04/exercicio09.js
@@ -1,48 +1,8 @@
-// const array1 = [1, 2, 3, 4,5];
+const lista = [2, 4, 6, 8]
+function soma(a, b){
+  return a + b;
+}
 
-// const initialValue = 0;
-// const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
-// );
+const total = lista.reduce(soma, 0);
 
-// console.log(sumWithInitial);
-
-// const array2 = [2, 4, 6,8];
-
-// const initialValue = 0;
-// const sumWithInitial = array2.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
-// );
-
-// console.log(sumWithInitial);
-
-// const array3 = [1, 3, 5, 7];
-
-// const initialValue = 0;
-// const sumWithInitial = array3.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
-// );
-
-// console.log(sumWithInitial);
-
-// const array4 = [0];
-
-// const initialValue = 0;
-// const sumWithInitial = array4.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
-// );
-
-// console.log(sumWithInitial);
-
-const array5 = [];
-const initialValue = 0;
-const sumWithInitial = array5.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  initialValue,
-);
-
-console.log(sumWithInitial);
+console.log(lista)
