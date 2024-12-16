9cfca860f17f92be7961898cc6f92b8b98289233
Bruna Silva <brunasilvaip2@gmail.com>
2024-12-16 15:18:21 -0300

    Reorganiza o exercicio10.js da lista 04

--- Exercicios/Exercicios/javaScript/lib/ExerciciosLista04/exercicio10.js
+++ Exercicios/Exercicios/javaScript/lib/ExerciciosLista04/exercicio10.js
@@ -1,12 +1,8 @@
-function simplestest(flag) {
-    let soma = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 === 0) { 
-            soma += arr[i];
-        }
-    }
-    return soma;
+function somaPares(arr) {
+    return arr.filter(num => num % 2 === 0)  
+              .reduce((soma, num) => soma + num, 0); 
 }
-console.log(somaPares([1, 2, 3, 4, 5]));  
-console.log(somaPares([2, 4, 6, 8]));     
-console.log(somaPares([1, 3, 5, 7]));     
+
+console.log(somaPares([1, 2, 3, 4, 5])); 
+console.log(somaPares([2, 4, 6, 8]));    
+console.log(somaPares([1, 3, 5, 7]));   
