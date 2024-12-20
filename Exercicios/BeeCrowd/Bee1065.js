1633944ab7f1d97f342825b2230bb86c7247153a
Bruna2015 <120494884+Bruna2015@users.noreply.github.com>
2024-12-20 09:10:41 -0300

    Bee1065.js

---
+++ Exercicios/BeeCrowd/Bee1065.js
@@ -0,0 +1,10 @@
+
+var input = require('fs').readFileSync('./input.txt', 'utf8');
+// console.log(input)
+var lines = input.trim().split('\n');
+
+
+const arr = [a, b, c, d, e] = lines.map(Number).filter((val) => val % 2 === 0)
+
+
+console.log(`${arr.length} valores pares`)
