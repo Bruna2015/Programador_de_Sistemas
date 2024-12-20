114677462026fceadb936ff01ec07c1730807e6c
Bruna2015 <120494884+Bruna2015@users.noreply.github.com>
2024-12-20 08:58:28 -0300

    Bee1050.js

---
+++ Exercicios/BeeCrowd/Bee1050.js
@@ -0,0 +1,25 @@
+
+var input = require('fs').readFileSync('./input.txt', 'utf8');
+// console.log(input)
+var lines = input.split(' ');
+ 
+var ddd = Number(lines[0])
+
+
+var dddCidades = {
+    61: 'Brasilia',
+    71: 'Salvador',
+    11: 'Sao Paulo', 
+    21: 'Rio de Janeiro',
+    32: 'Juiz de Fora',
+    19: 'Campinas',
+    27: 'Vitoria',
+    31: 'Belo Horizonte',
+    }
+
+
+
+
+    if (Object[key])
+        console.log(dddCidades[ddd])
+     {console.log("DDD nao cadastrado")}
